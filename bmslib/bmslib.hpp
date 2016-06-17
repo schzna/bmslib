b124514a869ef00e59073680e35823b338decf8d
yuki74w <16777216bit@gmail.com>
2016-06-17 15:31:22 +0900

    こんどこそ直しましたすんません

--- bmslib/bmslib.hpp
+++ bmslib/bmslib.hpp
@@ -201,7 +201,7 @@ namespace bmslib {
 						std::string rs;
 						for (int i = 1; i < strs.size() - 1; i++) {
 							rs += strs[i];
-							rs += "";
+							rs += " ";
 						}
 						bms.header.title = rs;
 					}
